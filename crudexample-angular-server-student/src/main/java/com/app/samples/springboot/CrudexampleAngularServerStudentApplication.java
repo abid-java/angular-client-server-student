ab33fb4cea1ee3c484c1e1fcff0f7e4fb8f745ab
abmohamm <mohd.92java@gmail.com>
2020-12-04 15:07:30 -0500

    [US1234]:Created Angular Server Skeleton Application

---
+++ crudexample-angular-server-student/src/main/java/com/app/samples/springboot/CrudexampleAngularServerStudentApplication.java
@@ -0,0 +1,13 @@
+package com.app.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudexampleAngularServerStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudexampleAngularServerStudentApplication.class, args);
+	}
+
+}
