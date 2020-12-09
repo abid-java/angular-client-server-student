1da6858fb390ac0f5370c69c92bccb88ae5d0ead
abmohamm <mohd.92java@gmail.com>
2020-12-08 20:42:11 -0500

    [US1234]:Created Customer Error Response Object and Global Exception Handler

--- crudexample-angular-server-student/src/main/java/com/app/samples/springboot/controller/StudentController.java
+++ crudexample-angular-server-student/src/main/java/com/app/samples/springboot/controller/StudentController.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.app.samples.springboot.util.StudentUtil;
 /**
  * The Class StudentController.
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/students")
 public class StudentController {
